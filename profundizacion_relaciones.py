41597e2f4f9f812ea49bb33abb8143aae8a70de6
johanarangel <68509096+johanarangel@users.noreply.github.com>
2020-11-03 13:44:36 -0400

    EJERCICIO TERMINADO
    COMPLETADO

--- profundizacion_relaciones.py
+++ profundizacion_relaciones.py
@@ -15,26 +15,29 @@ __author__ = "Johana Rangel"
 __email__ = "johanarang@hotmail.com"
 __version__ = "1.1"
 
-import sqlite3
+
 import csv
+import sqlite3
+
 
 def create_schema():
-     
-    conn = sqlite3.connect('libreria.db')
+    
+    conn = sqlite3.connect('biblioteca.db')
     c = conn.cursor()
 
     c.execute("""
-                DROP TABLE IF EXISTS autor;
+                DROP TABLE IF EXISTS escritor;
             """)
-
+    
     c.execute("""
                 DROP TABLE IF EXISTS libro;
             """)
 
+
     c.execute("""
-            CREATE TABLE autor(
+            CREATE TABLE escritor(
                 [id] INTEGER PRIMARY KEY AUTOINCREMENT,
-                [name] TEXT
+                [name] TEXT NOT NULL
             );
             """)
     
@@ -43,67 +46,144 @@ def create_schema():
                 [id] INTEGER PRIMARY KEY AUTOINCREMENT,
                 [title] TEXT NOT NULL,
                 [pags] INTEGER NOT NULL,
-                [fk_autor_id] INTEGER NOT NULL REFERENCES autor(id) 
+                [fk_author_id] INTEGER NOT NULL REFERENCES escritor(id)
             );
             """)
 
-   
     conn.commit()
     conn.close()
+
+def insert_libros():
+    pass
 
 def fill():
 
     with open('libreria_autor.csv', 'r') as archivo:
         data = list(csv.DictReader(archivo))
-    
-        autores = []
-        for x in range(len(data)):    
-            author = data[x]['autor']
-            autores.append((x+1,author))
-        
-    conn = sqlite3.connect('libreria.db')
-    conn.execute("PRAGMA foreign_keys= 1")
-    c = conn.cursor()
 
-    c.executemany("""
-        INSERT INTO autor (id, author)
-        VALUES(?,?);""", autores) 
+        autores = []
+        for x in range(len(data)):   
+            autor = data[x]['autor']
+            autores.append(autor)
+
+    conn = sqlite3.connect('biblioteca.db')
+    conn.execute("PRAGMA foreign_keys = 1") 
+    c = conn.cursor()
+    
+    
+    for x in autores:
+        c.execute("""
+            INSERT INTO escritor(name)
+            VALUES(?);
+            """, (x,))
 
     conn.commit()
     conn.close()
 
-with open('libreria_libro.csv', 'r') as archivo:
-    data = list(csv.DictReader(archivo))
 
-    libros = []
-    for x in range(len(data)):    
-        titulo = data[x]['titulo']
-        pags = int(data[x]['cantidad_paginas'])
-        author = data[x]['autor']
-        libros.append((x+1,titulo, pags, author))
+    with open('libreria_libro.csv', 'r') as archivo:
+        data = list(csv.DictReader(archivo))
     
-    conn = sqlite3.connect('libreria.db')
-    conn.execute("PRAGMA foreign_keys= 1")
+        libros = []
+        for x in range(len(data)):    
+            titulo = data[x]['titulo']
+            pags = int(data[x]['cantidad_paginas'])
+            author = data[x]['autor']
+            libros.append((titulo, pags, author))
+    
+    
+    conn = sqlite3.connect('biblioteca.db')
+    conn.execute("PRAGMA foreign_keys = 1")
     c = conn.cursor()
 
-    c.executemany("""
-        INSERT INTO libro (id, title, pags, fk_author_id)
-        SELECT ?,?,?, a.id
-        FROM autor as a
-        WHERE a.id = fk_author_id;""", libros)
+    try:
+        for x in libros:
+            c.execute("""
+                INSERT INTO libro (title, pags, fk_author_id)
+                SELECT ?,?, e.id
+                FROM escritor as e
+                WHERE e.name =?;""", x)
+    
+    except sqlite3.Error as err:
+        print(err)
 
+    conn.commit()
+    conn.close()
+
+def fetch(id):
+    
+    if id == 0:
+        
+        conn = sqlite3.connect('biblioteca.db')
+        conn.execute("PRAGMA foreign_keys = 1")
+        c = conn.cursor()
+        
+        c.execute("""SELECT * FROM libro;""")
+        
+        while True:
+            row = c.fetchone()
+            
+            if row is None:
+                break
+            
+            else:
+                print(row)
+
+        conn.commit()
+        conn.close()
+
+    elif id > 0:
+        
+        conn = sqlite3.connect('biblioteca.db')
+        conn.execute("PRAGMA foreign_keys = 1")
+        c = conn.cursor()
+
+        for row in c.execute("""SELECT title, pags, fk_author_id as autor_id FROM libro WHERE id=?;""", (id,)):
+            print('Fila:', id, 'resultado:', row)
+
+        c.execute("""
+                SELECT l.id, l.title, l.pags, e.name as name_autor
+                FROM libro as l, escritor as e 
+                WHERE l.fk_author_id = e.id;
+                """)
+        
+        while True:
+            row = c.fetchone()
+            
+            if row is None:
+                print('No existe fila para el id', id, 'ingresado')
+                break
+            
+            else:
+                print(row)
+          
+        conn.commit()
+        conn.close()  
+
+def search_author(book_title):
+    
+    conn = sqlite3.connect('biblioteca.db')
+    conn.execute("PRAGMA foreign_keys = 1")
+    c = conn.cursor()
+
+    for row in c.execute("""SELECT e.name 
+                            FROM libro as l, escritor as e
+                            WHERE l.fk_author_id = e.id AND title=?;""", (book_title,)):
+        return row
+        
+    
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
-  
-  create_schema()
-  #fill()
+    #------CREAR TABLA--------
+    create_schema()
+    
+    #---------LEER FILAS------
+    fill()
+    fetch(0) 
+    fetch(3)  
+    fetch(20) 
 
-#   # Leer filas 
-#   fetch()  # Ver todo el contenido de la DB
-#   fetch(3)  # Ver la fila 3
-#   fetch(20)  # Ver la fila 20
-
-#   # Buscar autor
-#   print(search_author('Relato de un naufrago'))
+    #---------BUSCAR AUTHOR POR TITLE-------- 
+    print(search_author('Relato de un naufrago'))
